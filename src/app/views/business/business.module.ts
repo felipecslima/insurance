5d3e685151bb71426c32ffc0712450177aa439fd
Joel Lima <felipe@collact.com.br>
2022-05-04 13:33:41 -0300

    fix: dep install and remove useless packages

--- src/app/views/business/business.module.ts
+++ src/app/views/business/business.module.ts
@@ -5,8 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { ChartsModule } from 'ng2-charts';
-import { FileUploadModule } from 'ng2-file-upload';
+
 import { SharedModule } from '../../shared/shared.module';
 
 import { MatDividerModule } from '@angular/material/divider';
@@ -17,7 +16,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 
-import { NgxMaskModule } from 'ngx-mask';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { ClinicPageComponent } from './pages/clinic-page/clinic-page.component';
@@ -25,14 +23,11 @@ import { BusinessRoutes } from './business.routing';
 
 @NgModule({
   imports: [
-    NgxMaskModule.forRoot(),
     CommonModule,
     FormsModule,
     MatProgressBarModule,
     FlexLayoutModule,
     NgxDatatableModule,
-    ChartsModule,
-    FileUploadModule,
     SharedModule,
     RouterModule.forChild(BusinessRoutes),
     MatDividerModule,
